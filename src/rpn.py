588dd2ed76e574ee53c68c8ff01216b942c9e70d
adronnie <khadkanabin862@gmail.com>
2025-10-01 15:10:11 +0900

    iuf

--- src/rpn.py
+++ src/rpn.py
@@ -1,5 +1,28 @@
-
-
 def eval_rpn(tokens):
-    # TODO: implement RPN evaluation using a stack
-    raise NotImplementedError
+    """
+    Evaluate a list of tokens in Reverse Polish Notation.
+
+    :param tokens: List of strings, e.g., ["2","1","+","3","*"]
+    :return: integer result of evaluation
+    """
+    stack = []
+    operators = {"+", "-", "*", "/"}  # Supported operators
+
+    for token in tokens:
+        if token in operators:
+            b = stack.pop()
+            a = stack.pop()
+            if token == "+":
+                stack.append(a + b)
+            elif token == "-":
+                stack.append(a - b)
+            elif token == "*":
+                stack.append(a * b)
+            elif token == "/":
+                # Integer division that truncates toward zero
+                stack.append(int(a / b))
+        else:
+            # token is a number
+            stack.append(int(token))
+
+    return stack[0]
